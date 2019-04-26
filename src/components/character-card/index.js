2444a289b08516260b3aa2e2bf57362f2d6bdf48
Thales Caricati <thales.caricati@gmail.com>
2019-04-26 18:49:19 -0300

    create cards list for characters

---
+++ src/components/character-card/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './character-card.css'
+
+export default () => (
+  <div className="character-card">
+    <div className="inner-card">
+      <figure>
+        <img />
+      </figure>
+      <h2>Name x</h2>
+    </div>
+  </div>
+)
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import CharacterCard from '../../components/character-card'
+import './home.css'
 
 export default () => (
-  <div>
-    <h1>Marvel</h1>
+  <div id="marvel">
+    <header id="header">
+      <img src="https://i.annihil.us/u/prod/misc/marvel.svg" alt="Marvel" />
+    </header>
+
+    <section className="page-wrapper container">
+      <header>
+        <h1>Marvel</h1>
+      </header>
+
+      <div className="card-list">
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
+        <CharacterCard />
+      </div>
+    </section>
   </div>
 )
