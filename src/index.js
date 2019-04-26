92482c6c0d037a08d9796d7ab2874a85d5de8185
Thales Caricati <thales.caricati@gmail.com>
2019-04-26 18:21:58 -0300

    clear files from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import Homepage from './pages/home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Homepage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/home/home.test.js
+++ src/pages/home/home.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Homepage from '.';
 
-it('renders without crashing', () => {
+it('renders homepage without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Homepage />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
