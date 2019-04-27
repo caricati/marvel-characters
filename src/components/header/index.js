0adce265c69b4724182ff189b35f09d12cf99e3a
Thales Caricati <thales.caricati@gmail.com>
2019-04-26 23:42:01 -0300

    abstract header on independent component

---
+++ src/components/header/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './header.css'
+
+export default () => (
+  <header id="header">
+    <img src="https://i.annihil.us/u/prod/misc/marvel.svg" alt="Marvel" />
+  </header>
+)
