de0d57cc1fc97f26ad0c03dcc200372bb7804173
Thales Caricati <thales.caricati@gmail.com>
2019-04-27 14:05:06 -0300

    button component support anchor and button element

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './button.css'
 
-export default ({ to, children }) => (
-  <a href={to} className="btn-primary">
-    <span className="btn-primary-inner">
-      {children}
-    </span>
-  </a>
-)
+export default function Button(props) {
+  const { to, children, onClick } = props
+  if(to) {
+    return (
+      <a href={to} onClick={onClick} className="btn-primary">
+        <span className="btn-primary-inner">
+          {children}
+        </span>
+      </a>
+    )
+  }
+
+  return (
+    <button onClick={onClick} className="btn-primary">
+      <span className="btn-primary-inner">
+        {children}
+      </span>
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  to: '',
+  children: null,
+  onClick: () => {},
+}
+
+Button.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+}
